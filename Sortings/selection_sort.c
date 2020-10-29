f459f4a2be118fccff7a33af05bec2e4cf67f6e6
Максим <56740030+Maxim1720@users.noreply.github.com>
2020-10-29 12:13:51 +0500

    Add files via upload

---
+++ Sortings/selection_sort.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+void selection_sort(int *Arr, int size)
+{
+	int max;
+	int max_index;
+	bool flag = false;
+	
+	while(size>1)
+	{
+		max = Arr[size-1];
+		max_index = size-1;
+		flag = true;
+		
+		
+		for(int i=0;i<size;i++)
+		{
+			if(Arr[i]>max)
+			{
+				max = Arr[i];
+				max_index = i;
+				flag = false;
+			}
+		}	
+		
+		if(!flag)
+		{
+			Arr[size-1]    ^= Arr[max_index];
+			Arr[max_index] ^= Arr[size-1];
+			Arr[size-1]    ^= Arr[max_index];
+		}
+		size--;
+	}
+}
+
+
+
+int main(int argc, char **argv)
+{
+	int Arr[10] = {0,15,9,1156,4,89,456,159,753,-620};
+	
+	selection_sort(Arr,10);
+	for(int i=0;i<10;i++)
+	{
+		printf("%d ",Arr[i]);
+	}	
+	return 0;
+}
+
